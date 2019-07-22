034eee3bb07e74026a7c7c50ed9fe38a3e62c2de
khuyen.tran <khuyen.tran@360life.vn>
2019-07-22 18:20:59 +0700

    test iframe va thay favicon

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -14,11 +14,11 @@ import {from} from 'rxjs';
     {selector: 'app-question', templateUrl: './question.component.html', styleUrls: ['./question.component.css']}
 )
 export class QuestionComponent implements OnInit {
-    @Input()question: Question;
-    @Input()user_id: String;
+    // @Input()question: Question;
+    // @Input()user_id: String;
 
-    votingRequest : VotingRequest;
-    options : Array<String>;
+    // votingRequest : VotingRequest;
+    // options : Array<String>;
 
     constructor(
         private userService : UserService,
@@ -29,64 +29,64 @@ export class QuestionComponent implements OnInit {
     ) {}
 
     ngOnInit(): void {
-        this.getQuestion();
-        console.log(this
-            .userService
-            .userID());
-        this.votingRequest = new VotingRequest();
-        this.votingRequest.user_id = this
-        .userService
-        .userID();
+        // this.getQuestion();
+        // console.log(this
+        //     .userService
+        //     .userID());
+        // this.votingRequest = new VotingRequest();
+        // this.votingRequest.user_id = this
+        // .userService
+        // .userID();
 
     }
 
-    getQuestion(): void {
-        const id = this
-            .route
-            .snapshot
-            .paramMap
-            .get('id');
-        this
-            .questionService
-            .getQuestion(id)
-            .subscribe(question => {
-                this.question = question;
-                this.votingRequest.voting_id = this.question.voting_id;
-                console.log(question)
-            });
-    }
+    // getQuestion(): void {
+    //     const id = this
+    //         .route
+    //         .snapshot
+    //         .paramMap
+    //         .get('id');
+    //     this
+    //         .questionService
+    //         .getQuestion(id)
+    //         .subscribe(question => {
+    //             this.question = question;
+    //             this.votingRequest.voting_id = this.question.voting_id;
+    //             console.log(question)
+    //         });
+    // }
 
-    vote(votes : VotingRequest): void {
-        if (this.votingRequest.user_id) {
-            this
-                .voteService
-                .vote(votes)
-                .subscribe(ms => {}); // xuất ra mã số dự thưởng của bạn là
-        } else {
-            this
-                .userService
-                .logIn();
-        }
-    }
+    // vote(votes : VotingRequest): void {
+    //     if (this.votingRequest.user_id) {
+    //         this
+    //             .voteService
+    //             .vote(votes)
+    //             .subscribe(ms => {}); // xuất ra mã số dự thưởng của bạn là
+    //     } else {
+    //         this
+    //             .userService
+    //             .logIn();
+    //     }
+    // }
 
-    chooseOption(id_option : String)
-    {
-        if(this.options)
-        {
-            this.options.push(id_option);
-        }
-        else
-        {
-            this.options = [id_option];
-        }
-        console.log("options ", this.options);
-    }
+    // chooseOption(id_option : String)
+    // {
+    //     if(this.options)
+    //     {
+    //         this.options.push(id_option);
+    //     }
+    //     else
+    //     {
+    //         this.options = [id_option];
+    //     }
+    //     console.log("options ", this.options);
+    // }
 
-    sendRequest()
-    {
-        this.votingRequest.options = this.options;
-        console.log(this.votingRequest);
-        this.vote(this.votingRequest);
-    }
+    // sendRequest()
+    // {
+    //     this.votingRequest.options = this.options;
+    //     console.log(this.votingRequest);
+    //     this.vote(this.votingRequest);
+    // }
 
 }
