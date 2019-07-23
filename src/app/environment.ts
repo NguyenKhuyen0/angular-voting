e6cea83313a3acebfeaed3ff26f94aa4dba52dfc
khuyen.tran <khuyen.tran@360life.vn>
2019-07-23 15:57:20 +0700

    thay doi domain

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -3,7 +3,7 @@ import { KeycloakConfig } from 'keycloak-angular'
 let keycloakConfig: KeycloakConfig = {
     url: 'https://id.360life.vn/auth/',
     realm: 'master',
-    clientId: 'idesign-dev'
+    clientId: 'voting-dev'
 }
 
 export const environment = {
@@ -12,11 +12,11 @@ export const environment = {
     SERVER: 'https://api.360life.vn/lostbird/',
     API: 'content/v1',
     NO_ADS: [2],
-    optionsUrlAPI: 'http://idesign.local:8000/api/v1/options',
-    questionsUrlAPI: 'http://idesign.local:8000/api/v1/questions',
-    votingUrlAPI: 'http://idesign.local:8000/api/v1/votings',
-    voteUrl:  'http://idesign.local:8000/api/v1/vote',
-    votesUrl: 'http://idesign.local:8000/api/v1/votes',
-    urlMedia: 'http://idesign.local:8000/images/',
+    optionsUrlAPI: 'http://voting.local:8000/api/v1/options',
+    questionsUrlAPI: 'http://voting.local:8000/api/v1/questions',
+    votingUrlAPI: 'http://voting.local:8000/api/v1/votings',
+    voteUrl:  'http://voting.local:8000/api/v1/vote',
+    votesUrl: 'http://voting.local:8000/api/v1/votes',
+    urlMedia: 'http://voting.local:8000/images/',
 }
 
